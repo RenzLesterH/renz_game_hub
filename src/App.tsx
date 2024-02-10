4df1bed52bf9cb024d63ad381d01230fc21ec088
Renz Halagao <rhalagao@village88.com>
2024-02-10 11:08:51 +0800

    Added navbar component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from '@chakra-ui/react'
+import NavBar from './components/navbar.component'
 
 function App() {
 
   return (
     <>
-      <Button colorScheme='blue'>Button</Button>
+      <Grid templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+      }}>
+        <GridItem area="nav">
+            <NavBar/>
+        </GridItem>
+        <Show above="lg">
+            <GridItem area="aside" bg="gold">Aside</GridItem>
+        </Show>
+        <GridItem area="main" bg="dodgerblue">main</GridItem>
+      </Grid>
     </>
   )
 }
