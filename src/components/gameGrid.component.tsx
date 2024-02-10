9145f4007ee69cf5e41a5fafb2d3582bf49c1155
Renz Halagao <rhalagao@village88.com>
2024-02-10 22:35:17 +0800

    Implement the card UI for game list.

--- src/components/gameGrid.component.tsx
+++ src/components/gameGrid.component.tsx
@@ -1,14 +1,18 @@
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGame.hook";
+import GameCard from "./gameCard.component";
 
 const GameGrid = () => {
     const {games, error } = useGames();    
 
     return (
         <>
-            {error && <p>{error}</p>}
-            <ul>
-                {games.map(game => <li>{game.name}</li>)}
-            </ul>
+            {error && <Text>{error}</Text>}
+            <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 5}} spacing={10} padding="10px">
+                {games.map(game => (
+                    <GameCard key={game.id} game={game}/>
+                ))}
+            </SimpleGrid>
         </>
     )
 }
