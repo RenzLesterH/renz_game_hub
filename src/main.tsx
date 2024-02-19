a8dcc8901ed6c77bcf150f6f3af3f041586cf467
Renz Halagao <rhalagao@village88.com>
2024-02-19 12:29:59 +0800

    Added react query.

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
-import theme from './theme.ts'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import theme from './theme.ts';
+import App from './App.tsx';
+import './index.css';
+
+const query_client = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <ChakraProvider theme={theme}>
-        <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
-          <App />
-      </ChakraProvider>
+      <QueryClientProvider client={query_client}>
+        <ChakraProvider theme={theme}>
+            <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
+            <App />
+        </ChakraProvider>
+      </QueryClientProvider>
   </React.StrictMode>,
 )
