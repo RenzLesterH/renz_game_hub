3fc9429816588c47b2da4350c279241ce613ec8b
Renz Halagao <rhalagao@village88.com>
2024-02-23 21:53:31 +0800

    Improve navigation bar functionality.

--- src/components/searchInput.component.tsx
+++ src/components/searchInput.component.tsx
@@ -2,16 +2,22 @@ import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
 import { useRef } from 'react';
 import { BsSearch } from 'react-icons/bs';
 import useGameQueryStore from '../store/gameQuery.store';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const SearchInput = () => {
     const ref = useRef<HTMLInputElement>(null);  
     const setSearch = useGameQueryStore(state => state.setSearch);
+    const location = useLocation();
+    const navigate = useNavigate();
 
     return (
         <form style={{width: "100%"}} onSubmit={(event) => {
             event.preventDefault();
             if(ref.current){
                 setSearch(ref.current.value);
+                if(location.pathname !== "/"){
+                    navigate("/");
+                }
             }
         }}>
             <InputGroup>
